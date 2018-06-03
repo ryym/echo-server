7b0db959337be87ac1105f212564f23eea7e007a
ryym <ryym.64@gmail.com>
2018-06-03 12:10:42 +0900

    Output logs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log(`receive: ${req.url}`);
   res.end(`${req.url}\n`);
 });
 
